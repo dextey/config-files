10269be91c71e2f68dd39146d0759b26e5edb969
Dexter <dexterdevmode@gmail.com>
2022-11-05 13:11:25 +0530

    Understanding Direct Acyclic Graph

---
+++ DataStructures/graph/adjacencyList.js
@@ -0,0 +1,43 @@
+class Node {
+  constructor(id, condition) {
+    this.id = id;
+    this.connectedTo = [];
+    this.condition = condition;
+    this.value = "";
+  }
+
+  connectNode(node) {
+    this.connectedTo.push(node.id);
+  }
+  changeValue(value) {
+    this.value = value;
+  }
+}
+
+class Graph {
+  constructor(nodes) {
+    this.nodes = [...nodes];
+  }
+
+  addNode(node) {
+    this.nodes.push(node);
+  }
+}
+
+const nodeA = new Node("A");
+const nodeB = new Node("B", "Avalue1");
+const nodeC = new Node("C", "Avalue2");
+const nodeD = new Node("D", "Bvalue3");
+const nodeE = new Node("E", "Cvalue4");
+
+const graph = new Graph([nodeA, nodeB, nodeC, nodeD, nodeE]);
+nodeB.connectNode(nodeA);
+nodeC.connectNode(nodeA);
+nodeD.connectNode(nodeB);
+nodeE.connectNode(nodeC);
+
+nodeA.changeValue("Avalue1");
+nodeB.changeValue("Bvalue3");
+
+console.log("Node : ", nodeA);
+console.log("Graph : ", JSON.stringify(graph, null, 4));
